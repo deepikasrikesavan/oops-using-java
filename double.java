024c266cc64a68ba16ffb02d51068933ba227c72
deepikasrikesavan <124652690+deepikasrikesavan@users.noreply.github.com>
2023-07-21 13:50:34 +0530

    Add files via upload

---
+++ double.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DOuble
+
+{
+
+public static void main(String[] args) {
+
+Scanner sc=new Scanner(System.in);
+
+System.out.print("Enter the number: ");
+
+int num = sc.nextInt();
+
+System.out.println(num<<1);
+
+}
+
+}
