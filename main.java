1a604458d7b82a4ae7613a78f44ee38ee84316e1
deepikasrikesavan <124652690+deepikasrikesavan@users.noreply.github.com>
2023-07-21 15:33:31 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,44 +1,19 @@
-abstract class Marks 
+import java.util.*;
+class main
 {
- public abstract float getPercentage();
-
+int h;
+int b;
+int d;
+main(
+{
+Scanner s=new Scanner(System.in);
+h=s.nextInt();
+b=s.nextInt();
+d=s.nextInt();
+int vol=h*b*d;
+System.out.print(vol);
 }
-class A extends Marks
- {
- private float marks1, marks2, marks3
-public A(float m1, float m2, float m3)
+public static void main(String[]args)
 {
- marks1 = m1;
- marks2 = m2;
- marks3 = m3;
+main m=new main();
 }
- public float getPercentage()
- {
- return ((marks1 + marks2 + marks3) / 300) * 100;
- }}
-class B extends Marks 
-{
- private float marks1, marks2, marks3, marks4;
- public B(float m1, float m2, float m3, float m4)
- { 
- marks1 = m1;
- marks2 = m2;
- marks3 = m3;
- marks4 = m4;
-}
- public float getPercentage() 
-{
-  return ((marks1 + marks2 + marks3 + marks4) / 400) * 100;
- }}
-
-public class Main 
-{
- public static void main(String[] args)
- {
-  A studentA = new A(80, 85, 90);
-  B studentB = new B(75, 80, 85, 90);
- System.out.println("Percentage of marks for student A: " + studentA.getPercentage() + "%");
- System.out.println("Percentage of marks for student B: " + studentB.getPercentage() + "%");
- }}
-
- 
